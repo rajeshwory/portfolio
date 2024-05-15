2a6d9bf4f7e29a3b6b6e0b501c8eed80a3843207
rajeshwory <shrestharajeshwory@gmail.com>
2024-05-15 19:12:54 +0545

    Add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const Header = () => {
+  return (
+    <section className="w-full">
+      <div className="flex flex-row justify-between items-center">
+        <a className=" text-4xl font-semibold">Rajeshwory Shrestha</a>
+        <div className="text-2xl gap-16 flex ">
+          <a className="hover:cursor-pointer hover:text-blue-300">About</a>
+          <a className="hover:cursor-pointer hover:text-blue-300">Projects</a>
+          <a className="hover:cursor-pointer hover:text-blue-300">Resume</a>
+          <a className="hover:cursor-pointer hover:text-blue-300">Skills</a>
+        </div>
+        {/* icon section */}
+        <div className="flex gap-10 justify-end">
+          <InstagramIcon className="text-white text-4xl hover:cursor-pointer hover:text-blue-300" />
+          <GitHubIcon className="text-white text-4xl hover:cursor-pointer hover:text-blue-300" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;

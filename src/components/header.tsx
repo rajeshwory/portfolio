bc6d60e46b66c37d13c672a41a76d5f82433197f
rajeshwory <shrestharajeshwory@gmail.com>
2024-05-17 13:07:12 +0545

    Make header component for the main page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,13 +3,14 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Header = () => {
   return (
-    <section className="w-full">
+    <section className="w-full sticky top-5 backdrop-blur-3xl p-2">
       <div className="flex flex-row justify-between items-center">
         <a className=" text-4xl font-semibold">Rajeshwory Shrestha</a>
-        <div className="text-2xl gap-16 flex ">
-          <a className="hover:cursor-pointer hover:text-blue-300">About</a>
+        <div className="text-2xl gap-16 flex">
+          <a className="hover:cursor-pointer hover:text-blue-300" href="#about">
+            About
+          </a>
           <a className="hover:cursor-pointer hover:text-blue-300">Projects</a>
-          <a className="hover:cursor-pointer hover:text-blue-300">Resume</a>
           <a className="hover:cursor-pointer hover:text-blue-300">Skills</a>
         </div>
         {/* icon section */}

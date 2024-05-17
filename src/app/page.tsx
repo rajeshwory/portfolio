1af5033385742ee5fb1b9b83e4b67c4e52730f59
rajeshwory <shrestharajeshwory@gmail.com>
2024-05-17 13:05:52 +0545

    Add components to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+import About from "@/components/about";
 import Header from "@/components/header";
+import HeroSection from "@/components/hero-section";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
     <main
-      className="relative bg-cover flex flex-1 object-contain bg-left h-[100vh] z-10 p-10"
+      className="relative flex flex-1  min-h-screen z-10 p-10 flex-col"
       style={{
-        backgroundImage: 'url("background.jpg")',
-        backgroundColor: "black",
+        backgroundImage: "url('background.jpg')",
       }}
     >
       <div className="absolute inset-0 bg-black opacity-70 -z-10" />
+
       <Header />
+      <HeroSection />
+      <About />
+      <Projects />
     </main>
   );
 }

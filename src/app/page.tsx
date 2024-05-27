0c84603445157724082c8976f4dfd47efd5cf75f
rajeshwory <shrestharajeshwory@gmail.com>
2024-05-27 17:31:31 +0545

    Add skill component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/about";
 import Header from "@/components/header";
 import HeroSection from "@/components/hero-section";
 import Projects from "@/components/projects";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       <HeroSection />
       <About />
       <Projects />
+      <Skills />
     </main>
   );
 }

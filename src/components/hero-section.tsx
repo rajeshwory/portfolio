7ad2a026c27d333cba775dde5ce22ed605ecd7a1
Rajeshwory Shrestha <shrestharajeshwory@gmail.com>
2025-07-23 05:51:39 -0400

    Add profile image to hero section and update image source

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -7,9 +7,7 @@ const HeroSection = () => {
     <section className="w-full mt-32 h-screen">
       <div className="flex flex-col content-center items-center">
         <img
-          src={
-            "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
+          src="profile.jpg"
           alt={"profile"}
           className="w-36 h-36 border-white border-4 object-cover object-top rounded-full"
         />
